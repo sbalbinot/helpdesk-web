a3e3e22db77ee2449f81969acb1977fb2150d25f
stephano <stephanobalbinot@hotmail.com>
2018-06-21 00:30:05 -0300

    Implementação do layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
